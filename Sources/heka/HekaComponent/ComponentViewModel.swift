86e8549791eb3a5ef5d0a9d1beb9aa1641901554
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-05 01:38:59 +0530

    call create connection before sync ios health data

--- Sources/heka/HekaComponent/ComponentViewModel.swift
+++ Sources/heka/HekaComponent/ComponentViewModel.swift
@@ -1,15 +1,15 @@
-  //
-  //  ComponentViewModel.swift
-  //
-  //
-  //  Created by Gaurav Tiwari on 04/02/23.
-  //
+//
+//  ComponentViewModel.swift
+//
+//
+//  Created by Gaurav Tiwari on 04/02/23.
+//
 
 import Combine
 import UIKit
 
 final class ComponentViewModel: ObservableObject {
-  
+
   @Published private var state = ConnectionState.notConnected
   @Published var errorOccured = Bool()
   private(set) var errorDescription = String() {
@@ -19,30 +19,30 @@ final class ComponentViewModel: ObservableObject {
       }
     }
   }
-  
+
   var uuid: String
   var apiKey: String
   private let apiManager: APIManager
   private let hekaManager = HekaManager()
-  
+
   init(uuid: String, apiKey: String) {
     self.uuid = uuid
     self.apiKey = apiKey
     apiManager = APIManager(apiKey: apiKey)
   }
-  
+
   var buttonTitle: String {
     state.buttonTitle
   }
-  
+
   var isSyncStatusLabelHidden: Bool {
     state.isSyncingLabelHidden
   }
-  
+
   var currentConnectionState: ConnectionState {
     state
   }
-  
+
   var buttonBGColor: UIColor {
     state.buttonBGColor
   }
@@ -54,7 +54,7 @@ extension ComponentViewModel {
       self.state = newState
     }
   }
-  
+
   func checkConnectionStatus() {
     apiManager.fetchConnection(user_uuid: uuid) { connection in
       guard connection != nil else {
@@ -64,38 +64,47 @@ extension ComponentViewModel {
       self.setState(to: .connected)
     }
   }
-  
+
   func checkHealthKitPermissions() {
     hekaManager.requestAuthorization { allowed in
       if allowed {
         self.setState(to: .syncing)
+        self.makeRequestToWatchSDK {
+          success in
+          if success {
+            self.setState(to: .connected)
+          } else {
+            self.setState(to: .notConnected)
+            self.errorDescription = "Failed to make a connection"
+            return
+          }
+        }
         self.hekaManager.syncIosHealthData(
           apiKey: self.apiKey, userUuid: self.uuid
         ) { success in
           if success {
-            self.makeRequestToWatchSDK()
           } else {
             self.setState(to: .notConnected)
             self.errorDescription = "Unable to sync health data"
           }
         }
       } else {
-        self.errorDescription = "Please allow health app access permission, in order to use this widget"
+        self.errorDescription =
+          "Please allow health app access permission, in order to use this widget"
       }
     }
   }
-  
-  private func makeRequestToWatchSDK() {
+
+  private func makeRequestToWatchSDK(completion: @escaping (Bool) -> Void) {
     apiManager.makeConnection(
       userUuid: uuid, platform: "apple_healthkit",
       googleFitRefreshToken: nil, emailId: nil
     ) { result in
       switch result {
-        case .success:
-          self.setState(to: .connected)
-        case .failure(let failure):
-          self.setState(to: .notConnected)
-          self.errorDescription = failure.localizedDescription
+      case .success:
+        completion(true)
+      case .failure(let failure):
+        completion(false)
       }
     }
   }
