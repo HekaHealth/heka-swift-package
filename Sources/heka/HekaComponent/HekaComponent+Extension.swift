c29d63c8126e4bb292a0555bc0926ff8c564464e
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-04 14:24:04 +0530

    link observer query and fix platform name

--- Sources/heka/HekaComponent/HekaComponent+Extension.swift
+++ Sources/heka/HekaComponent/HekaComponent+Extension.swift
@@ -1,14 +1,14 @@
-  //
-  //  HekaComponent+Extension.swift
-  //
-  //
-  //  Created by Gaurav Tiwari on 03/02/23.
-  //
+//
+//  HekaComponent+Extension.swift
+//
+//
+//  Created by Gaurav Tiwari on 03/02/23.
+//
 
 import UIKit
 
 extension HekaComponent {
-  
+
   func checkConnectionStatus() {
     guard let uuid = userUUID else {
       fatalError("user's UUID not set for the HekaComponent")
@@ -18,42 +18,49 @@ extension HekaComponent {
         print("Not Connected")
         return
       }
-      
+
       self.state = .connected
       print(connection.connectedPlatforms)
     }
   }
-  
+
   func checkHealthKitPermissions() {
     guard hekaManager.checkHealthKitPermissions() else {
       self.presentAlert(with: "Allow health data permissions in the Seetings App")
       return
     }
-    
-      //TODO: - Setup observer query
+
+    hekaManager.syncIosHealthData(
+      apiKey: apiManager?.apiKey, userUuid: userUUID,
+      completion: { success in
+        if success {
+        } else {
+          self.presentAlert(with: "Unable to sync health data")
+        }
+      })
     apiManager?.makeConnection(
-      userUuid: userUUID!, platform: "iOS",
+      userUuid: userUUID!, platform: "apple_healthkit",
       googleFitRefreshToken: nil, emailId: nil
     ) { result in
       switch result {
-        case .success(let connection):
-          self.state = .connected
-          print(connection.connectedPlatforms)
-        case .failure(let failure):
-          self.presentAlert(with: failure.localizedDescription)
+      case .success(let connection):
+        self.state = .connected
+        print(connection.connectedPlatforms)
+      case .failure(let failure):
+        self.presentAlert(with: failure.localizedDescription)
       }
     }
   }
-  
+
   private func presentAlert(with message: String) {
     let alert = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
     let action = UIAlertAction(title: "OK", style: .default)
     alert.addAction(action)
     let keyWindow = UIApplication.shared.connectedScenes
-      .filter({$0.activationState == .foregroundActive})
-      .compactMap({$0 as? UIWindowScene})
+      .filter({ $0.activationState == .foregroundActive })
+      .compactMap({ $0 as? UIWindowScene })
       .first?.windows
-      .filter({$0.isKeyWindow}).first
+      .filter({ $0.isKeyWindow }).first
     keyWindow?.rootViewController?.present(alert, animated: true)
   }
 }
