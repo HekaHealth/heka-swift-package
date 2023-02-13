199f8b3abdbdd02e71ebf0b2399a15fefc802ec0
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-13 13:57:16 +0530

    fix

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "heka",
-      dependencies: ["Alamofire", "PromiseKit", "SwiftyJSON"],
+      dependencies: ["Alamofire", "PromiseKit"],
       path: "Sources"
     ),
     .testTarget(
